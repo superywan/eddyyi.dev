97518aa9c86eabf51056a49a01d21e0cd60ca963
Eddy Yi <superywan@Eddys-MacBook-Pro.local>
2021-04-07 16:38:59 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+// Dark Mode Toggle
+const toggle = document.querySelector(".toggle");
+const theme = document.querySelector("#theme-link");
+const themeEmoji = document.querySelector(".toggle__img");
+
+toggle.addEventListener("click", (e) => {
+  if (theme.getAttribute("href") === "/styles/light-theme.css") {
+    theme.href = "/styles/dark-theme.css";
+    themeEmoji.src = "/img/emojis/new-moon-face_1f31a.png";
+  } else {
+    theme.href = "/styles/light-theme.css";
+    themeEmoji.src = "/img/emojis/full-moon-face_1f31d.png";
+  }
+});
+
+// Set theme by user's local time
+window.addEventListener("load", (e) => {
+  const currentDate = new Date();
+  const onlyTime = currentDate.getHours();
+  if (onlyTime > 8 && onlyTime < 17) {
+    theme.href = "/styles/light-theme.css";
+    themeEmoji.src = "/img/emojis/full-moon-face_1f31d.png";
+  } else {
+    theme.href = "/styles/dark-theme.css";
+    themeEmoji.src = "/img/emojis/new-moon-face_1f31a.png";
+  }
+});
+
+// Fade-In Animation Trigger
+const faders = document.querySelectorAll(".fade-in");
+const sectionOneOptions = {
+  rootMargin: "0px 0px -350px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.add("appear");
+    appearOnScroll.unobserve(entry.target);
+  });
+}, sectionOneOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+// Hide Scroll to Top Button when it's on top
+const topBtn = document.querySelector("#top-button");
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 0) {
+    topBtn.classList.remove("hide");
+    topBtn.classList.add("show");
+  } else {
+    topBtn.classList.remove("show");
+    topBtn.classList.add("hide");
+  }
+});
+
+// Scroll to the Top when Top Button is clicked
+topBtn.addEventListener("click", (e) => {
+  window.scrollTo(0, 0);
+});
